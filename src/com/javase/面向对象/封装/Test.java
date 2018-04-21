d2830eb62c01191657d43617e1e29e40c12fe508
724888 <362294931@qq.com>
2018-04-21 20:46:11 +0800

    Java 三大特性

---
+++ src/com/javase/面向对象/封装/Test.java
@@ -0,0 +1,13 @@
+package com.javase.面向对象.封装;
+import com.javase.面向对象.封装.World.*;
+/**
+ * Created by 周杰伦 on 2018/4/21.
+ */
+public class Test {
+    public static void main(String[] args) {
+        World world = new World();
+        System.out.println(world.pro);
+        System.out.println(world.pub);
+
+    }
+}
---
+++ src/com/javase/面向对象/封装/World.java
@@ -0,0 +1,36 @@
+package com.javase.面向对象.封装;
+
+/**
+ * Created by 周杰伦 on 2018/4/21.
+ */
+public class World {
+    private int pri;
+    protected int pro;
+    public int pub;
+
+    public int getPri() {
+        return pri;
+    }
+
+    public void setPri(int pri) {
+        this.pri = pri;
+    }
+
+    public int getPro() {
+        return pro;
+    }
+
+    public void setPro(int pro) {
+        this.pro = pro;
+    }
+
+    public int getPub() {
+        return pub;
+    }
+
+    public void setPub(int pub) {
+        this.pub = pub;
+    }
+}
+
+
